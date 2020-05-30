be5cab864dd242782f518c0057daa63b362ec2bd
MagicByJay <magic.by.jay.dev@gmail.com>
2020-05-30 11:05:00 +0000

    add tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
   // copy extra site files to output direcotry
 
   eleventyConfig.addPassthroughCopy({ "frontend/reveal": "reveal" });
+  eleventyConfig.addPassthroughCopy({ "frontend/css": "css" });
 };
