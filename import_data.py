c5349e3df5f057c58729e483aba5f1349dfc4c86
MagicByJay <magic.by.jay.dev@gmail.com>
2020-06-15 13:27:25 +0000

    data format and import

--- import_data.py
+++ import_data.py
@@ -1,5 +1,11 @@
-import xmltodict, json
+import xmltodict, json, glob
 
-with open('test.xml') as inFh:
-    with open('sample.json','w') as outFh:
-        json.dump(xmltodict.parse(inFh.read()), outFh)
+path = 'import_data/'
+
+files = [f for f in glob.glob(path + '*.xml',recursive=True)]
+
+for f in files:
+    with open(f) as inFh:
+        fname = f.split('.')
+        with open('hold_data/' + fname[0].split('/')[1] + '.json','w') as outFh:
+            json.dump(xmltodict.parse(inFh.read()), outFh)
